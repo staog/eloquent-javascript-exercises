498330b4011d18eb53e811575685741231012345
Milan V. Kecojević <staog@gmx.com>
2019-02-13 20:39:00 +0100

    Update is-even.js

--- is-even.js
+++ is-even.js
@@ -1,3 +1,6 @@
+// Solution for positive numbers only
+// If number provided has negative value, you'll get this error: "InternalError: too much recursion[Learn More]"
+
 var isEven = function(number) {
   if (number < 0) {
     return "Please, provide a positive number.";
@@ -12,3 +15,19 @@ var isEven = function(number) {
 };
 
 isEven(50);
+
+// Solution for all numbers, positive or negative
+
+/* var isEven = function(number) {
+  if (number == 0) {
+    return true;
+  } else if (number == 1 || number == -1) {
+      return false;
+  } else if (number >= 2) {
+      return isEven(number - 2);
+  } else {
+      return isEven(number + 2);
+  }      
+};
+
+isEven(50); */
