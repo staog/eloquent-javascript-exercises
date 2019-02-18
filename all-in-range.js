2ce8780fe9751fbb6af28a47b326ba04dd1a0562
Milan V. Kecojević <staog@gmx.com>
2019-02-18 19:57:55 +0100

    Update all-in-range.js

--- all-in-range.js
+++ all-in-range.js
@@ -1,3 +1,5 @@
+// This function returns all numbers in range from x to (and including) y
+
 var allInRange = function(x, y) {
   var arr = [];
   for (var i = x; i <= y; i++) {
@@ -8,3 +10,14 @@ var allInRange = function(x, y) {
 
 console.log(allInRange(1, 10));
 
+// This function takes array as an argument and returns sum of all numbers in that array
+
+var sumAllNumbers = function(arr) {
+  var counter = 0;
+  for (var i = 0; i < arr.length; i++) {
+    counter += arr[i];
+  }
+  return counter;
+}
+
+console.log(sumAllNumbers(allInRange(1, 10)));
